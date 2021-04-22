130dee66766c94f36b36daa0c63da66c18d49700
Tobias Fischer <info@tobiasfischer.info>
2021-04-22 15:36:43 +1000

    Fixes for gps scripts

--- scripts/extract_gps.py
+++ scripts/extract_gps.py
@@ -9,18 +9,30 @@ from tqdm.auto import tqdm
 with rosbag.Bag('/home/tobias/bags_2020-08-07-09-06-59.bag') as bag:
     num_msgs = bag.get_message_count('/gps/fix')
     with tqdm(total=num_msgs) as pbar:
-        latitudes, longitudes = [], []
+        latitudes, longitudes, altitudes, times = [], [], [], []
+        valid_samples = 0
+        all_samples = 0
 
         for topic, msg, t in bag.read_messages():
             if topic == '/gps/fix':
                 if not np.isnan(msg.latitude) and not np.isnan(msg.longitude):
-                    latitudes.append(msg.latitude)
-                    longitudes.append(msg.longitude)
+                    if msg.status.status != 0 or msg.status.service != 1:
+                        print(msg.status.status, msg.status.service)
+                    # print(msg.position_covariance)
+                    if msg.position_covariance[0] < 15:
+                        valid_samples = valid_samples + 1
+                        latitudes.append(msg.latitude)
+                        longitudes.append(msg.longitude)
+                        altitudes.append(msg.altitude)
+                        times.append(msg.header.stamp.to_sec())
+                    all_samples = all_samples + 1
                     # print(msg.latitude)
                 pbar.update(1)
 
         latitudes = np.array(latitudes)
         longitudes = np.array(longitudes)
-        print('saved', len(latitudes), 'samples')
+        times = np.array(times)
+        altitudes = np.array(altitudes)
+        print('saved', valid_samples, 'valid samples out of', all_samples, 'total samples')
 
-        np.savez('gps.npz', latitudes=latitudes, longitudes=longitudes)
+        np.savez('gps.npz', latitudes=latitudes, longitudes=longitudes, times=times, altitudes=altitudes)
--- scripts/gps_to_gpx.py
+++ scripts/gps_to_gpx.py
@@ -1,26 +1,57 @@
 import numpy as np
-import gpxpy
 import gpxpy.gpx
+import os
+# from lxml import etree as ElementTree
 
 gps = np.load('gps.npz')
 lats = gps['latitudes'].tolist()
 lons = gps['longitudes'].tolist()
+times = gps['times'].tolist()
+altitudes = gps['altitudes'].tolist()
 
-gpx = gpxpy.gpx.GPX()
+last_time = None
 
-# Create first track in our GPX:
-gpx_track = gpxpy.gpx.GPXTrack()
-gpx.tracks.append(gpx_track)
+if not os.path.isdir('gps_out'):
+    os.makedirs('gps_out')
 
-# Create first segment in our GPX track:
-gpx_segment = gpxpy.gpx.GPXTrackSegment()
-gpx_track.segments.append(gpx_segment)
+# parser = ElementTree.XMLParser(recover=True)
+# gpx_extension_color = ElementTree.fromstring("""<gpxx:TrackExtension xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
+#     <gpxx:DisplayColor>Black</gpxx:DisplayColor>
+#     </gpxx:TrackExtension>
+# """, parser)
+# print(gpx_extension_color)
+
+gps_num = 0
 
 # Create points:
-for lat, lon in zip(lats, lons):
-    gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon))
+for lat, lon, altitude, time in zip(lats, lons, altitudes, times):
+    if last_time is None or (time - last_time > 0.5 and len(gpx_segment.points) > 0):
+        if last_time is not None:
+            print('Add new segment')
+            with open('gps_out/gps_%05d.gpx' % gps_num, 'w') as f:
+                f.write(gpx.to_xml())
+            gps_num = gps_num + 1
 
-print('Created GPX:', gpx.to_xml())
+        gpx = gpxpy.gpx.GPX()
+        # gpx.nsmap["gpxtpx"] = "http://www.garmin.com/xmlschemas/GpxExtensions/v3"
 
-with open('gps.gpx', 'w') as f:
-    f.write(gpx.to_xml())
+        # Create first track in our GPX:
+        gpx_track = gpxpy.gpx.GPXTrack()
+        gpx.tracks.append(gpx_track)
+
+        # Create first segment in our GPX track:
+        gpx_segment = gpxpy.gpx.GPXTrackSegment()
+        gpx_track.segments.append(gpx_segment)
+
+        # print('Created GPX:', gpx.to_xml())
+
+    gpx_point = gpxpy.gpx.GPXTrackPoint(lat, lon, altitude)
+    gpx_segment.points.append(gpx_point)
+    # gpx_point.extensions.append(gpx_extension_color)
+    last_time = time
+
+# print('Created GPX:', gpx.to_xml())
+
+if len(gpx_segment.points) > 0:
+    with open('gps_out/gps_%05d.gpx' % gps_num, 'w') as f:
+        f.write(gpx.to_xml())
